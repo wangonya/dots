d1085f6d2f0a8d045ea8c55e16414c80572aa872
kelvin <kelvin@jenga-agency.com>
2021-06-15 18:57:15 +0300

    add org+presentations setup

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -25,3 +25,34 @@
 ;; ignore venv from lsp watcher
 (with-eval-after-load 'lsp-mode
   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\venv\\'"))
+
+;; presentations
+(global-set-key (kbd "<f8>") 'org-tree-slide-mode)
+(global-set-key (kbd "S-<f8>") 'org-tree-slide-skip-done-toggle)
+(setq org-tree-slide-activate-message "Presentation started!")
+(setq org-tree-slide-deactivate-message "Presentation finished!")
+(setq org-tree-slide-header t)
+(setq org-tree-slide-breadcrumbs " > ")
+(setq org-image-actual-width nil)
+(setq org-tree-slide-cursor-init nil)
+(with-eval-after-load "org-tree-slide"
+  (define-key org-tree-slide-mode-map (kbd "<f9>") 'org-tree-slide-move-previous-tree)
+  (define-key org-tree-slide-mode-map (kbd "<f10>") 'org-tree-slide-move-next-tree))
+
+(defun presentation-setup ()
+
+  ;; Display images inline
+  (org-display-inline-images) ;; Can also use org-startup-with-inline-images
+
+  ;; Scale the text.  The next line is for basic scaling:
+  (setq text-scale-mode-amount 3)
+  (text-scale-mode 1))
+
+(defun presentation-end ()
+
+  ;; Turn off text scale mode (or use the next line if you didn't use text-scale-mode)
+  (text-scale-mode 0))
+
+(add-hook 'org-tree-slide-play-hook 'presentation-setup)
+(add-hook 'org-tree-slide-stop-hook 'presentation-end)
+(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
