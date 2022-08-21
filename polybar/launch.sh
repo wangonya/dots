4094e555013c6ca90d569326e86f4aa759abc9a1
wangonya <kwangonya@gmail.com>
2022-08-21 22:37:56 +0300

    multimonitor setting

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,4 +4,11 @@ killall -q polybar
 
 CONFIG_DIR=$HOME/dots/polybar/config.ini
 
+# if [[ $(xrandr -q | grep "${EXTERNAL_MONITOR} connected") ]]; then
+#     MONITOR=HDMI-1
+# else
+#     MONITOR=eDP-1
+# fi
+# MONITOR=$MONITOR polybar main -c $CONFIG_DIR &
+
 polybar main -c $CONFIG_DIR &
